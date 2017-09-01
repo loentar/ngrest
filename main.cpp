330f75bd040d1a02b7a95460f52d5b23de94534f
Carolina <carolina.linero@hotmail.com>
2017-09-01 00:48:57 -0300

    Organización de archivos y carpetas. Librerías libpq.lib y otras añadidas.

--- main.cpp
+++ main.cpp
@@ -10,12 +10,61 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <libpq-fe.h>
+
 
 using namespace std;
+
+PGconn *cnn = NULL;
+PGresult *result = NULL;
+
+char *host = "localhost";
+char *port = "5432";
+char *dataBase = "apaaa";//char *dataBase = "postgres";
+char *user = "postgres";
+char *passwd = "1234";
 
  int _tmain(int argc, _TCHAR* argv[]) 
 {
 	cout << "Hello world" << endl;
+     int i;
+
+    cnn = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);
+    if (PQstatus(cnn) != CONNECTION_BAD) {
+        cout << "Estamos conectados a PostgreSQL!" << endl;
+       /* result = PQexec(cnn, "SELECT * FROM users");//result = PQexec(cnn, "SELECT * FROM test");
+
+        if (result != NULL) {
+            int tuplas = PQntuples(result);
+            int campos = PQnfields(result);
+            cout << "No. Filas:" << tuplas << endl;
+            cout << "No. Campos:" << campos << endl;
+
+            cout << "Los nombres de los campos son:" << endl;
+
+            for (i=0; i<campos; i++) {
+                cout << PQfname(result,i) << " | ";
+            }
+
+            cout << endl << "Contenido de la tabla" << endl;
+
+            for (i=0; i<tuplas; i++) {
+                for (int j=0; j<campos; j++) {
+                    cout << PQgetvalue(result,i,j) << " | ";
+                }
+                cout << endl;
+            }
+        }
+*/
+        // Ahora nos toca liberar la memoria
+        PQclear(result);
+
+    } else {
+        cout << "Error de conexion" << endl;
+        return 0;
+    }
+
+	PQfinish(cnn);
 
     system("pause");
 	return 0;
